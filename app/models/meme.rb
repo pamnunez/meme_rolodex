b9b3367ba0cb63b0000bac480ad7e8a2f1e8628c
pamnunez <nunezpam@gmail.com>
2024-01-08 18:37:48 -0500

    Updated uniqueness validations

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,6 +1,6 @@
 class Meme < ApplicationRecord
   validates :title, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, uniqueness: { scope: :user_id }
 
   belongs_to :user
 
--- app/models/meme_tagging.rb
+++ app/models/meme_tagging.rb
@@ -1,6 +1,6 @@
 class MemeTagging < ApplicationRecord
   validates :meme_id, presence: true
-  validates :tag_id, presence: true
+  validates :tag_id, presence: true, uniqueness: { scope: :meme_id }
 
   belongs_to :meme
   belongs_to :tag
