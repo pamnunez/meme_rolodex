52f95ddacf63711fff712c30c7bbe8eb4590b990
pamnunez <nunezpam@gmail.com>
2024-01-09 20:06:27 -0500

    Added functionality to save data, data not being saved?

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,8 +1,24 @@
 class MemesController < ApplicationController
   def index
+    @memes = Meme.all
   end
 
   def new
+    @meme = Meme.new
+  end
 
+  def create
+    @meme = Meme.new(meme_params)
+
+    if @meme.save
+      redirect_to @meme
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def meme_params
+    params.require(:meme).permit(:title, :url)
   end
 end
