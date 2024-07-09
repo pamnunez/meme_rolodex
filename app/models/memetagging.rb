410c541970ac27b1bfd13bd01fd27a7c453b13f9
Ryan Vasios <rvasios@wrapbook.com>
2024-07-09 14:12:20 -0400

    Rename memetagging file and classname

--- app/models/memetagging.rb
+++ app/models/memetagging.rb
@@ -1,4 +1,4 @@
-class MemeTagging < ApplicationRecord
+class Memetagging < ApplicationRecord
   validates :meme_id, presence: true
   validates :tag_id, presence: true, uniqueness: { scope: :meme_id }
 
